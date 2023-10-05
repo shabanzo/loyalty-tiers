12d36eefa73e20f7cfcb560c69bb47b60f87dccc
shabanzo <rizkyshaban@gmail.com>
2023-10-05 17:09:08 +0700

    API - Set default tier list

--- api/app/models/tier.rb
+++ api/app/models/tier.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class Tier < ApplicationRecord
+  # Defaults:
+  # id: 1, name: Bronze, min_spent_cents: 0
+  # id: 2, name: Silver, min_spent_cents: 10_000
+  # id: 3, name: Gold, min_spent_cents: 50_000
+
   has_many :loyalty_stats
 end
--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Tier.create(id: 1, name: 'Bronze', min_spent_cents: 0) if Tier.find(1).blank?
+Tier.create(id: 2, name: 'Silver', min_spent_cents: 10_000) if Tier.find(2).blank?
+Tier.create(id: 3, name: 'Gold', min_spent_cents: 50_000) if Tier.find(3).blank?
