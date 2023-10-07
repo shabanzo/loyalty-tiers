80e96b24e3f1e2c94a7f19f7da9bd8cc77071b06
shabanzo <rizkyshaban@gmail.com>
2023-10-07 18:19:59 +0700

    API - Populate dummy orders

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -8,6 +8,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts '=== Generating Tier List ==='
 Tier.create(id: 1, name: 'Bronze', min_spent_cents: 0) if Tier.where(id: 1).blank?
 Tier.create(id: 2, name: 'Silver', min_spent_cents: 10_000) if Tier.where(id: 2).blank?
 Tier.create(id: 3, name: 'Gold', min_spent_cents: 50_000) if Tier.where(id: 3).blank?
+
+if Rails.env == 'development'
+  puts '=== Generating Dummy Orders ==='
+  FactoryBot.create_list(
+    :completed_order,
+    50,
+    customer_id: 1
+  )
+end
