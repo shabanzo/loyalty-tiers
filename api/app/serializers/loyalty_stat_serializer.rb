7a03a3ec843ccf9e187bf02c1103c8c6ce56c721
shabanzo <rizkyshaban@gmail.com>
2023-10-07 13:13:19 +0700

    API - Add comments to the serializers to explain each attribute

--- api/app/serializers/loyalty_stat_serializer.rb
+++ api/app/serializers/loyalty_stat_serializer.rb
@@ -6,6 +6,7 @@ class LoyaltyStatSerializer < ActiveModel::Serializer
              :remaining_amount_to_upgrade, :upgrade_to
 
   def total_spent_cents
+    # This year total spent in cents + last year total spent in cents
     @total_spent_cents ||= object.total_spent_cents + prev_stats_total_spents
   end
 
@@ -14,34 +15,48 @@ class LoyaltyStatSerializer < ActiveModel::Serializer
   end
 
   def start_date
+    # First date in the last year
     Date.new(Date.current.prev_year.year, 1, 1)
   end
 
   def downgrade_to
+    # Return nil if the customer has spent enought this year to retain the current tier
     return nil if retain_the_current_tier?
 
+    # Tier below the current tier by querying:
+    # Tier.min_spent_cents <= this_year_stats.total_spent_cents
+    # Order it by ID ASC and take last one
     downgrade_tier.name
   end
 
   def downgrade_date
+    # Last day in the current year
     Date.new(Date.current.year, 12, 31)
   end
 
   def remaining_amount_to_retain
+    # Return zero if the customer has spent enough this year to retain the current tier
     return 0 if retain_the_current_tier?
 
+    # Min spending in cents for the current tier - total spending within this year
     current_tier.min_spent_cents - object.total_spent_cents
   end
 
   def remaining_amount_to_upgrade
+    # Return 0 if the customer reached the highest tier
+
+    # Min spending in cents for the next tier - total spending since the start_date
     next_tier ? next_tier.min_spent_cents - total_spent_cents : 0
   end
 
   def upgrade_to
+    # Return nil if the customer reached the highest tier
+
+    # Next tier name
     next_tier&.name.presence
   end
 
-  # ==== Below are helpers ====
+  # ==== Helpers! ==== #
 
   def prev_stats
     @prev_stats ||= ::LoyaltyStat.where(
